57d173b05e8d53b00e6f09b133506dc654090111
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2025-01-22 23:34:41 +0530

    Update Basic.dart

--- Basic.dart
+++ Basic.dart
@@ -1,7 +1,10 @@
 void main() {
+  // string property
   String value = '3564d5';
   print(value.length);
+
   
+  //using shortcuts for operations using operators
   int first = 20;
   print(first);
   
@@ -10,13 +13,58 @@ void main() {
   
   first -= 75;
   print(first);
-  
+
+
+  // String manipulation/updation
   String greeting = 'Hello World';
   print(greeting);
   
   greeting = greeting + ' YOOO';
   print(greeting);
-//   or use string interpolation
-  greeting = '${greeting} ,YOO';
+                
+  greeting = '$greeting ,YOO';            // use string interpolation
   print(greeting);
+
+  greeting = '${greeting.length} , YOO';        // Brackets to be used in string interpolation only when property is also defined with the variable
+  print(greeting);
+
+  greeting = '\$23';                  // To prine $ in console or screen '\$--' is used
+  print(greeting);
+  
+  greeting = '''Hello world ,          
+  how are you''';                        // For multiline code ''' - - - ''' is used
+  print(greeting);
+  
+  greeting = 'Why are you so happy\nwhy???';                  // '\n' is used as enter/move to next line
+  print(greeting);
+
+  
+  // var/final/const variableName = value
+  var first = '32';                    // mutable variable
+  const second = '7364';               // immutable compile time variable
+  final third = 387;                   // immutable runtime variable
+  
+  print(first);
+  print(second);
+  print(third);
+  
+  print('----------');
+  first = '63';
+  
+  print(first);
+  print(second);
+  print(third);
+
+  // final is runtime variable 
+  final someValue = DateTime.now();        // Determines and store the current date and time
+  print(someValue);
+
+  // optional variable /null variable
+  // string/int/bool and null
+  String? someValue2 = null;               // For nullable variable use ? afyter the variable type
+  int? some = null;
+
+  print(someValue2);
+  print(some);
+  
 }

8058cd7914b1f10aee262f1bac2e77bca37804f4
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2025-01-22 22:56:30 +0530

    Create Basic.dart

---
+++ Basic.dart
@@ -0,0 +1,22 @@
+void main() {
+  String value = '3564d5';
+  print(value.length);
+  
+  int first = 20;
+  print(first);
+  
+  first += 75;
+  print(first);
+  
+  first -= 75;
+  print(first);
+  
+  String greeting = 'Hello World';
+  print(greeting);
+  
+  greeting = greeting + ' YOOO';
+  print(greeting);
+//   or use string interpolation
+  greeting = '${greeting} ,YOO';
+  print(greeting);
+}

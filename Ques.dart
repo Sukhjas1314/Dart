7590d38736b8f5e22239cf79bf435fb5094192ee
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2025-01-23 23:24:32 +0530

    Update Ques.dart

--- Ques.dart
+++ Ques.dart
@@ -1,18 +1,35 @@
-//  Q.  Destination is given and accordingly calculate the shipping cost of item whose cost is given per kg
+//  Q1.  Destination is given and accordingly calculate the shipping cost of item whose cost is given per kg
 
 void main() {
   String destinationZone = 'PQR';
   double weight = 65.7;
+
+  //  With if-else
   if(destinationZone == 'XYZ'){
-    print('The shipping cost is \$${weight*5}');
+    print('Shipping cost : \$${weight*5}');
   }
   else if(destinationZone == 'ABC'){
-    print('The shipping cost is \$${weight*7}');
+    print('Shipping cost : \$${weight*7}');
   }
   else if(destinationZone == 'PQR'){
-    print('The shipping cost is \$${weight*10}');
+    print('Shipping cost : \$${weight*10}');
   }
   else{
     print('Error');
+    return;
+  }
+
+
+
+  //  With Switch statement 
+  switch(destinationZone){
+    case 'XYZ' :
+      print('Shipping cost : \$${weight*5}');
+    case 'ABC' :
+      print('Shipping cost : \$${weight*7}');
+    case 'PQR' :
+      print('Shipping cost : \$${weight*10}');
+    default :
+      print('Error');
   }
 }

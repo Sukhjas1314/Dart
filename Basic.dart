dc90de5e033e3b8215a9260777372a33d729d998
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2025-01-30 22:37:11 +0530

    Update Basic.dart

--- Basic.dart
+++ Basic.dart
@@ -1031,3 +1031,18 @@ void main(){
     
 
 
+// Exception Handling
+void main(){
+  print(10/3);
+  print(10~/3);
+  print(10/0);
+  
+  try{
+    print(10~/0);
+  }
+  catch(e){
+    print('Some Error occured');
+  }
+  print('Sukhman');
+}
+

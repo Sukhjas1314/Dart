ed6ebf63e52611847e4173584ace3a0b75734b34
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2025-01-31 23:19:17 +0530

    Update Basic.dart

--- Basic.dart
+++ Basic.dart
@@ -1085,4 +1085,17 @@ Future<String> givesResultAfter2sec()  {
 }
 
 
+
+// Importing external api's
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() async {
+  var url = Uri.https('jsonplaceholder.typicode.com', 'users/1');
+  final res = await http.get (url);
+  
+  print(jsonDecode(res.body)['name']);
+}
+
+
   

10b90723785d14bdeb3953f70ad179b787e6b7e6
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2025-02-05 23:05:49 +0530

    Update Basic.dart

--- Basic.dart
+++ Basic.dart
@@ -1197,3 +1197,13 @@ void main(){
   };
   print(text);
 }
+
+
+
+
+// Capitalise the first letter of the sentence
+void main(){
+  String motivation = 'this is a good world';
+  motivation = motivation[0].toUpperCase() + motivation.substring(1);
+  print(motivation);  
+}
